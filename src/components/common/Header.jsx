3febb22818a08dac021d8cc313cc9e608f3e5e62
Didier Nsengiyumva <nsedidier@gmail.com>
2025-04-11 16:28:22 +0200

    latest updates showing screens 428

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -24,7 +24,7 @@ const Header = () => {
       <div className="container mx-auto px-4 py-3">
         <div className="flex items-center justify-between">
           <Link to="/" className="text-2xl font-bold text-indigo-600 dark:text-indigo-400">
-            Marketplace
+            Ruzindana
           </Link>
 
           <div className="hidden md:block flex-1 mx-8">
